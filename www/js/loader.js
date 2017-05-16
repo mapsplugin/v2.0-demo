d966b46d372e5e65669f1fcdcb18d3835d2dfab8
wf9a5m75 <wf9a5m75@gmail.com>
2017-05-16 10:52:12 -0700

    Update: example code of Map.getMap() and Map.setOptions()

--- www/js/loader.js
+++ www/js/loader.js
@@ -1,14 +1,19 @@
 document.addEventListener("deviceready", function() {
   var pTags = document.getElementsByClassName("demo");
   for (var i = 0; i < pTags.length; i++) {
-    executeCode(pTags[i]);
+    executeCode(pTags[i], 500 * i);
   }
 }, false);
 
-function executeCode(pTag) {
+function executeCode(pTag, loadDelay) {
   var code = pTag.getElementsByTagName("pre")[0];
   //console.log(code.innerText);
-  eval(code.innerText);
+
+  // In order to prevent freezing the device (because too much work),
+  // waiting kind of times is better when you use the multiple maps in one page.
+  setTimeout(function() {
+    eval(code.innerText);
+  }, loadDelay);
 }
 
 function showVirtualDialog(parentDiv, message) {
